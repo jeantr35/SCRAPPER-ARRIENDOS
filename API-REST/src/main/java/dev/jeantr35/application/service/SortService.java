1e9b261a08f1560126555d476a4015babfcf1c1e
jeantr35 <jean.trujillo.35@gmail.com>
2022-11-28 23:15:08 -0500

    Add mailer to sort service

--- API-REST/src/main/java/dev/jeantr35/application/service/SortService.java
+++ API-REST/src/main/java/dev/jeantr35/application/service/SortService.java
@@ -5,6 +5,8 @@ import dev.jeantr35.domain.models.ApartmentInfo;
 import dev.jeantr35.domain.models.CityToScrapCommand;
 import dev.jeantr35.infrastructure.repositories.CityToSrapCommandRepository;
 import dev.jeantr35.infrastructure.repositories.BestChoicesRepository;
+import io.quarkus.mailer.Mail;
+import io.quarkus.mailer.Mailer;
 import org.bson.types.ObjectId;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -16,11 +18,15 @@ import java.util.stream.Collectors;
 
 @ApplicationScoped
 public class SortService {
+    public static final String SUBJECT_FORMAT = "Ya está listo tu informe para la ciudad de %s con las mejores opciones";
     @Inject
     public CityToSrapCommandRepository cityToSrapCommandRepository;
 
     @Inject
     public BestChoicesRepository bestChoicesRepository;
+
+    @Inject
+    public Mailer mailer;
 
     private final ArrayList<Comparator<ApartmentInfo>> comparators = new ArrayList<>();
 
@@ -42,7 +48,7 @@ public class SortService {
 
 
 
-    public Response executeSort(String id){
+    public Response executeSortAndThenNotify(String id){
         createComparator();
         CityToScrapCommand cityToScrapCommand = cityToSrapCommandRepository.findById(new ObjectId(id));
         final Comparator<ApartmentInfo>[] apartmentInfoComparator = new Comparator[]{comparators.stream().findFirst().isPresent() ? comparators.stream().findFirst().get() : null};
@@ -54,6 +60,7 @@ public class SortService {
                     limit(5).collect(Collectors.toList()));
                 });
         bestChoicesRepository.persist(bestChoicesDTO);
+        this.notifyUser(cityToScrapCommand.getEmailToNotify(), String.format(SUBJECT_FORMAT, cityToScrapCommand.getCity()), bestChoicesDTO.getId().toString());
         return Response.ok(bestChoicesDTO.getId()).build();
     }
 
@@ -62,6 +69,10 @@ public class SortService {
         comparators.add(priceComparatorMRef);
         comparators.add(toiletsComparatorMRef);
         comparators.add(areaComparatorMRef);
+    }
+
+    private void notifyUser(String to, String subject, String idToLink){
+        mailer.send(Mail.withText(to, subject, "This is my body. " + idToLink)); //TODO: Cambiar por el link a la pagina
     }
 
 }
--- API-REST/src/main/java/dev/jeantr35/infrastructure/controller/SortController.java
+++ API-REST/src/main/java/dev/jeantr35/infrastructure/controller/SortController.java
@@ -3,21 +3,21 @@ package dev.jeantr35.infrastructure.controller;
 import dev.jeantr35.application.service.SortService;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
-@Path("/sort")
+@Path("/sort-and-notify")
 public class SortController {
 
     @Inject
     public SortService sortService;
 
-    @GET
+    @POST
     @Path("/{id}")
-    public Response getSortedValues(@PathParam("id") String id){
-        return sortService.executeSort(id);
+    public Response sortAndNotify(@PathParam("id") String id){
+        return sortService.executeSortAndThenNotify(id);
     }
 
 }
